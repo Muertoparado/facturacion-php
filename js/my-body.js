5894652c948396add85ad3e52e361072be2746c0
Muertoparado <nicolas.casanova20@gmail.com>
2023-06-05 09:28:22 -0500

    feat: :sparkles: nuevo componente dentro de body de productos

--- js/my-body.js
+++ js/my-body.js
@@ -1,16 +1,17 @@
 import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
-export class myBody extends HTMLElement{
-    constructor(){
+export class myBody extends HTMLElement {
+    constructor() {
         super();
+        document.adoptedStyleSheets.push(styles);
     }
-    async components(){
+    async components() {
         return await (await fetch("view/my-body.html")).text();
     }
-    connectedCallback(){
-        document.adoptedStyleSheets.push(styles);
-        this.components().then(html=>{
+   
+    connectedCallback() {
+        this.components().then(html => {
             this.innerHTML = html;
         })
     }
 }
-customElements.define('my-body',myBody);
+customElements.define('my-body', myBody);
---
+++ js/my-productDetail.js
@@ -0,0 +1,40 @@
+import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
+export class myProductDetails extends HTMLElement{
+    constructor(){
+        super();
+        document.adoptedStyleSheets.push(styles);
+    }
+    async components() {
+        return await (await fetch("view/my-productDetails.html")).text();
+    }
+    selection(e) {
+        let $ = e.target;
+        if ($.nodeName == "BUTTON") {
+            let inputs = document.querySelectorAll(`#${$.dataset.row} input`);
+            if ($.innerHTML == "-") {
+                inputs.forEach(element => {
+                    if (element.name == "amount" && element.value == 0) {
+                        document.querySelector(`#${$.dataset.row}`).remove();
+                    } else if (element.name == "amount") {
+                        element.value--;
+                    }
+                });
+            } else if ($.innerHTML == "+") {
+                inputs.forEach(element => {
+                    if (element.name == "amount") {
+                        element.value++;
+                    }
+                });
+            }
+        }
+    }
+    
+    connectedCallback(){
+        this.components().then(html => {
+            this.innerHTML = html;
+            this.container = document.querySelector("#products");
+            this.container.addEventListener("click", this.selection);
+        })
+    }
+}
+customElements.define("my-product-details", myProductDetails);
